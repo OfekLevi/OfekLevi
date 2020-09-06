86e56b3159434e7456f7a51d04d61d40a935bb52
Yuval Schnitzer <yuvo88@gmail.com>
2020-09-06 08:57:07 +0300

    Add project files.

---
+++ OfekLevi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OfekLevi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
